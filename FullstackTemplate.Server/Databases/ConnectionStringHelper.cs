1c3d0903383c65558ecf73a76a434ee3647f623b
Paul DeVito <pdevito3@gmail.com>
2026-01-03 23:07:11 -0500

    docs

--- FullstackTemplate.Server/Databases/ConnectionStringHelper.cs
+++ FullstackTemplate.Server/Databases/ConnectionStringHelper.cs
@@ -2,6 +2,34 @@ namespace FullstackTemplate.Server.Databases;
 
 using Npgsql;
 
+/// <summary>
+/// Corrects Northflank's malformed PostgreSQL connection string for use with .NET's Npgsql driver.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This helper exists to work around two issues with the POSTGRES_URI provided by Northflank:
+/// </para>
+/// <list type="number">
+///   <item>
+///     <term>Format mismatch</term>
+///     <description>
+///       Northflank provides URIs in PostgreSQL format (postgresql://user:pass@host:port/db),
+///       but .NET's Npgsql driver expects ADO.NET format (Host=...;Username=...;Password=...).
+///     </description>
+///   </item>
+///   <item>
+///     <term>Malformed query string</term>
+///     <description>
+///       The URI ends with ?sslmode with no value (e.g., .../_13a9c48d44a3?sslmode).
+///       It should be ?sslmode=require or omitted entirely. This causes parsing failures
+///       even with Npgsql's built-in URI parser.
+///     </description>
+///   </item>
+/// </list>
+/// <para>
+/// If Northflank fixes their connection string format or you don't need this correction, this helper can be removed.
+/// </para>
+/// </remarks>
 public static class ConnectionStringHelper
 {
     /// <summary>
